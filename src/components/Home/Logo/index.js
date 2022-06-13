cf6ba951721e503a88b7e2f5e600e4e45f76dfa6
Chivas1717 <markgud1717@gmail.com>
2022-06-13 00:56:53 -0400

    without logo

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,4 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoM from '../../../assets/images/logo-m.png'
 import './index.scss'
 
@@ -9,43 +7,11 @@ const Logo = () => {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-    console.log(bgRef.current)
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 2,
-        duration: 4,
-      }
-    )
-  }, [])
-
   return (
     <div className="logo-container" ref={bgRef}>
-      <img
-        className="solid-logo"
-        ref={solidLogoRef}
-        src={LogoM}
-        alt="JavaScript,  Developer"
-      />
+      <img className="solid-logo" src={LogoM} alt="JavaScript,  Developer" />
 
-      <svg
+      {/* <svg
         width="559pt"
         height="897pt"
         version="1.0"
@@ -57,12 +23,12 @@ const Logo = () => {
           transform="translate(0 457) scale(.1 -.1)"
           fill="none"
         >
-          {/* <path
+          <path
             ref={outlineLogoRef}
             d="m177,68l106,-1c0,0 39,114 39,114c0,0 42,-115 42,-115c0,0 106,0 106,0c0,0 74,324 74,324c0,0 -110,0 -110,0c0,0 -29,-137 -29,-137c0,0 -48,141 -48,141c0,0 -67,0 -67,0c0,0 -48,-137 -48,-137c0,0 -30,133 -30,133c0,0 -107,0 -107,0c0,0 72,-322 72,-322z"
-          /> */}
+          />
         </g>
-      </svg>
+      </svg> */}
     </div>
   )
 }
