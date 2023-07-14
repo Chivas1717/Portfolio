1fbfc6683fb1c2fdc1c1dca0c34a14d23dc4b8ff
Chivas1717 <markgud1717@gmail.com>
2023-07-13 22:02:56 -0400

    cv updated

--- src/components/Home/CVpdf/CV.js
+++ src/components/Home/CVpdf/CV.js
@@ -1,16 +1,19 @@
-import CVimg from '../../../assets/images/img.png'
+import CVimg from '../../../assets/images/CV.jpg'
 import './index.scss'
 import File from '../../../assets/pdf/CV.pdf'
 
 const CV = () => {
   return (
-    <div className='wrapper'>
-      <div className='card-cv'>
-        <img src={CVimg} alt='CV' className='img-cv'/>
-        <button className='download-btn'><a href={File} target="_blank" download="CV-PDF-document">Download CV</a></button>
+    <div className="wrapper">
+      <div className="card-cv">
+        <img src={CVimg} alt="CV" className="img-cv" />
+        <button className="download-btn">
+          <a href={File} target="_blank" download="CV-PDF-document">
+            Download CV
+          </a>
+        </button>
       </div>
     </div>
-
   )
 }
 
